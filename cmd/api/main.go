b1cc0739073a2a50392bf73a3dc3667819b9d0bd
Jiří Martínek <zwo@centrum.cz>
2022-07-18 15:51:35 +0200

    Intercepting Shutdown Signals.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
 	"github.com/OnkelPony/greenlight/internal/data"
 	"github.com/OnkelPony/greenlight/internal/jsonlog"
 	_ "github.com/lib/pq"
-	"log"
-	"net/http"
 	"os"
 	"time"
 )
@@ -73,20 +70,10 @@ func main() {
 		logger: logger,
 		models: data.NewModels(db),
 	}
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
+	err = app.serve()
+	if err != nil {
+		logger.PrintFatal(err, nil)
 	}
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
-	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {

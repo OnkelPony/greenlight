3c64987f9da369cf8a98c3d34fcf26da43120632
Jiří Martínek <zwo@centrum.cz>
2022-07-25 16:01:44 +0200

    Sending Activation Tokens.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -5,6 +5,7 @@ import (
 	"github.com/OnkelPony/greenlight/internal/data"
 	"github.com/OnkelPony/greenlight/internal/validator"
 	"net/http"
+	"time"
 )
 
 func (app *application) registeUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,8 +45,17 @@ func (app *application) registeUserHandler(w http.ResponseWriter, r *http.Reques
 		}
 		return
 	}
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.gohtml", user)
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.gohtml", data)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
